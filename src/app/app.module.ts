fc17c31a63ebcde860d2ffbade0d0ae95e035c68
pozasl <loic.pozas@monlaboweb.com>
2024-07-24 12:16:12 +0000

    [ADD] Basic OlympicPie component implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { OlympicPieComponent } from "./components/olympic-pie/olympic-pie.component";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, OlympicPieComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/olympic-pie/olympic-pie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-olympic-pie',
+  standalone: true,
+  imports: [],
+  templateUrl: './olympic-pie.component.html',
+  styleUrl: './olympic-pie.component.scss'
+})
+export class OlympicPieComponent {
+
+}
