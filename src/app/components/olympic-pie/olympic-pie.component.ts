c10d93bebe2de1cdd9feb733e3f176eaba59ef47
pozasl <loic.pozas@monlaboweb.com>
2024-07-25 10:39:54 +0000

    [ADD] Ngx-charts PieChart component integration

--- src/app/components/olympic-pie/olympic-pie.component.ts
+++ src/app/components/olympic-pie/olympic-pie.component.ts
@@ -1,14 +1,47 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Olympic } from 'src/app/core/models/Olympic';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { PieChartData } from 'src/app/core/models/PieChartData';
 
 @Component({
   selector: 'app-olympic-pie',
   standalone: true,
-  imports: [],
+  imports: [NgxChartsModule],
   templateUrl: './olympic-pie.component.html',
   styleUrl: './olympic-pie.component.scss'
 })
 export class OlympicPieComponent {
-  @Input() olympics: Olympic[] | null = [];
+  @Input() set olympics(ol: Olympic[] | null) {
+   this.chartData = (ol !== null) ? ol.map(this.convertOlympicToPieChartData) : []
+  }
+  // options
+  gradient: boolean = true;
+  showLegend: boolean = false;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+  legendPosition: string = 'below';
+  chartData: PieChartData[] = [];
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  convertOlympicToPieChartData(olympic: Olympic): PieChartData {
+    return Object.assign({
+      name: olympic.country,
+      value: olympic.participations.reduce((tot, current) => tot + current.medalsCount, 0)
+    }) as PieChartData
+  }
   
+  onSelect(data:any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data:any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data:any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
 }
---
+++ src/app/core/models/PieChartData.ts
@@ -0,0 +1,11 @@
+/*
+  {
+      "name": "Germany",
+      "value": 40632
+    }
+*/
+
+export interface PieChartData {
+  name: string;
+  value: number;
+}
