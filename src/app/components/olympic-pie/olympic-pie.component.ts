712837eb6ff0aa7c1c9584ac0b427294801b305a
pozasl <loic.pozas@monlaboweb.com>
2024-07-25 07:31:43 +0000

    [ADD] @Input & string interpolation

--- src/app/components/olympic-pie/olympic-pie.component.ts
+++ src/app/components/olympic-pie/olympic-pie.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Olympic } from 'src/app/core/models/Olympic';
 
 @Component({
   selector: 'app-olympic-pie',
@@ -8,5 +9,6 @@ import { Component } from '@angular/core';
   styleUrl: './olympic-pie.component.scss'
 })
 export class OlympicPieComponent {
-
+  @Input() olympics: Olympic[] | null = [];
+  
 }
