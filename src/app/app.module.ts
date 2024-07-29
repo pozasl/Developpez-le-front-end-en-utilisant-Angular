87b502ecfe3bc283ceaf0514a549dff3cb1f9541
pozasl <loic.pozas@monlaboweb.com>
2024-07-29 14:55:42 +0000

    [FIX] deprecated HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,11 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent],
-  imports: [BrowserAnimationsModule, BrowserModule, AppRoutingModule, HttpClientModule, HomeComponent],
-  providers: [],
+  imports: [BrowserAnimationsModule, BrowserModule, AppRoutingModule, HomeComponent],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
